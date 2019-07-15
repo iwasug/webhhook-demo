0ff7facd1a79024dff95323bf9b940bf2dde1a23
Iwa Sugriwa <41350556+iwasug@users.noreply.github.com>
2019-07-15 11:09:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,8 +29,6 @@ if($method == 'POST'){
 
 	$response = new \stdClass();
 	$response->fullfillmentText = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
 	echo json_encode($response);
 }
 else
